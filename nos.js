5cb148bde92fb5ee2e5566581a6b25b2f5ed8621
EricCooigo <Eric.zh24@gmail.com>
2015-01-06 19:30:34 +0800

    调整配置
    调整配置

--- nos.js
+++ nos.js
@@ -26,26 +26,40 @@ fis.config.merge({
             less: 'css'
         },
         path : [
-			{
-                reg : "README.md",
-                release : false
-            },
             {
                 //一级同名组件，可以引用短路径，比如modules/jquery/juqery.js
                 //直接引用为var $ = require('jquery');
-                reg : /^\/js\/modules\/([^\/]+)\/\1\.(js)$/i,
+                reg: /^\/modules\/([^\/]+)\/\1\.(js)$/i,
                 //是组件化的，会被jswrapper包装
-                isMod : true,
+                isMod: true,
                 //id为文件夹名
-                id : '$1',
+                id: '$1',
+                release: '$&'
             },
             {
                 //modules目录下的其他脚本文件
-                reg : /^\/js\/modules\/(.*)\.(js)$/i,
+                reg: /^\/modules\/(.*)\.(js)$/i,
                 //是组件化的，会被jswrapper包装
-                isMod : true,
+                isMod: true,
                 //id是去掉modules和.js后缀中间的部分
-                id : '$1',
+                id: '$1',
+                release: '$&'
+            },
+            {
+                reg: /^\/obj\/.*$/i,
+                release: false
+            },
+            {
+                reg: /.*\.(cs|csproj|suo|sln|user)$/i,
+                release: false
+            },
+			{
+			    reg: "README.md",
+			    release: false
+			},
+            {
+                reg: "**",
+                release: '$&'
             }
         ]
     },
